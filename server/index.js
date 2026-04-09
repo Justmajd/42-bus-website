aa1e6190b5f18f6072234925673e47060244256e
Majd jamal <115942742+Justmajd@users.noreply.github.com>
2026-04-09 20:02:49 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -45,13 +45,7 @@ app.get('/api/qr/:tripId', authenticateToken, requireAdmin, async (req, res) => 
   res.json({ qr_data_url: qrDataUrl, qr_token: trip.qr_token });
 });
 
-// Serve static files in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/index.html'));
-  });
-}
+
 
 // Start server
 app.listen(PORT, '0.0.0.0', () => {
